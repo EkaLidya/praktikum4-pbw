0d5ed019ab746afcdac769ad4d103cfa47a3c868
EkaLidya <lidyae6@gmail.com>
2025-10-17 16:07:29 +0700

    Initial commit for fresh Laravel 11 project

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+<title>Kontak - LaraPress</title> 
+</head> 
+<body> 
+<h1>Tentang LaraPress</h1> 
+<p>Email: lidyae6@gmail.com dan Nomor Telephone: 081311263781.</p> 
+<a href="/">Lihat Halaman Kontak</a> 
+</body> 
+</body> 
+</html> 
